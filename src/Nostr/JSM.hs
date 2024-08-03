903c86db81b0f48d376725f500a246cec0129d30
branko@branko <branko@branko>
2024-08-03 09:03:27 +0200

    first

---
+++ src/Nostr/JSM.hs
@@ -0,0 +1,103 @@
+{-# LANGUAGE CPP                        #-}
+{-# LANGUAGE OverloadedStrings          #-}
+
+module Nostr.JSM where 
+
+import           Miso.FFI
+import Miso.FFI.WebSocket as WS
+import Miso.Effect
+import Miso.Subscription.WebSocket
+import Data.Aeson
+import           GHCJS.Marshal
+import           GHCJS.Foreign
+import           GHCJS.Types ()
+import Control.Monad
+import Control.Monad.IO.Class
+import Miso.String
+import Control.Concurrent
+
+createWebSocket :: MisoString -> [MisoString] -> JSM Socket
+{-# INLINE createWebSocket #-}
+createWebSocket url' protocols =
+  WS.create url' =<< toJSVal protocols
+
+codeToCloseCode :: Int -> CloseCode
+codeToCloseCode = go
+  where
+    go 1000 = CLOSE_NORMAL
+    go 1001 = CLOSE_GOING_AWAY
+    go 1002 = CLOSE_PROTOCOL_ERROR
+    go 1003 = CLOSE_UNSUPPORTED
+    go 1005 = CLOSE_NO_STATUS
+    go 1006 = CLOSE_ABNORMAL
+    go 1007 = Unsupported_Data
+    go 1008 = Policy_Violation
+    go 1009 = CLOSE_TOO_LARGE
+    go 1010 = Missing_Extension
+    go 1011 = Internal_Error
+    go 1012 = Service_Restart
+    go 1013 = Try_Again_Later
+    go 1015 = TLS_Handshake
+    go n    = OtherCode n
+
+secs :: Int -> Int
+secs = (*1000000)
+
+sendJson' :: ToJSON json => Socket -> json -> JSM ()
+sendJson' socket m = WS.send socket =<< stringify m
+
+send :: ToJSON a => Socket -> a -> JSM ()
+{-# INLINE send #-}
+send s x = do
+  sendJson' s x
+
+
+startWebSocketSubscriptions
+  :: FromJSON m
+  => URL
+  -> Protocols
+  -> (WebSocket m -> action)
+  -> Sub action
+startWebSocketSubscriptions (URL u) (Protocols ps) f sink = do
+--   void . forkJSM $ handleReconnect
+  pure ()
+
+  where
+    initRelay relay = do
+      socket <- createWebSocket u ps
+      WS.addEventListener socket "open" $ \_ -> liftIO $ do
+        sink (f WebSocketOpen)
+      WS.addEventListener socket "message" $ \v -> do
+        d <- parse =<< WS.data' v
+        liftIO . sink $ f (WebSocketMessage d)
+      WS.addEventListener socket "close" $ \e -> do
+        code <- codeToCloseCode <$> WS.code e
+        -- liftIO (writeIORef closedCode (Just code))
+        reason <- WS.reason e
+        clean <- WS.wasClean e
+        liftIO . sink $ f (WebSocketClose code clean reason)
+      WS.addEventListener socket "error" $ \v -> do
+        -- liftIO (writeIORef closedCode Nothing)
+        d' <- WS.data' v
+#ifndef ghcjs_HOST_OS
+        undef <- ghcjsPure (isUndefined d')
+#else
+        let undef = isUndefined d'
+#endif
+        if undef
+          then do
+            liftIO . sink $ f (WebSocketError mempty)
+          else do
+            Just d <- fromJSVal d'
+            liftIO . sink $ f (WebSocketError d)
+
+    -- handleReconnect = do
+    --   liftIO (threadDelay (secs 3))
+    --   Just s <- liftIO (readIORef websocket)
+    --   status <- WS.socketState s
+    --   code <- liftIO (readIORef closedCode)
+    --   if status == 3
+    --     then do
+    --       unless (code == Just CLOSE_NORMAL) $
+    --         websocketSub (URL u) (Protocols ps) f sink
+    --     else handleReconnect
