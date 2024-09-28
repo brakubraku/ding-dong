3bea2436e9b855a75e250b90353b28911b81bec3
branislav <branislav>
2024-09-28 09:46:21 +0200

    modified dependencies now in their repos instead of in local directories

--- datetime-0.3.1/test/Suite.hs
+++
@@ -1,8 +0,0 @@
-module Main where
-
-import  Test.Framework (defaultMain)
-
-import qualified Data.DateTimeTest
-
-main :: IO ()
-main = defaultMain $ Data.DateTimeTest.tests
--- jsaddle-0.9.9.2/src-ghc/Data/JSString/Internal.hs
+++
@@ -1,1 +0,0 @@
-module Data.JSString.Internal where
