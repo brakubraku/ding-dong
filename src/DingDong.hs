f4582ab2d4e135caa81db00d6574cb337a5eb8ee
branislav <branislav>
2024-11-07 15:37:16 +0100

    keep the fancier missing event display for later

--- src/DingDong.hs
+++ src/DingDong.hs
@@ -1372,8 +1372,7 @@ displayThread :: Model -> Event -> View Action
 displayThread m e =
   let reid = RootEid $ fromMaybe (e ^. #eventId) $ findRootEid e
       isWriteReply = Just e == m ^. #writeReplyTo
-      ep = newEvent "<Parent event not found on connected relays>" (m ^. #me) (m ^. #now) 
-      parentNotFound = div_ [class_ "parent-not-found"] [displayNote m (ep, processContent ep)]
+      parentNotFound = div_ [class_ "parent-not-found"] [text "Loading parent or parent not found on connected relays..."]
       parentDisplay = maybe  (bool Nothing (Just parentNotFound) $ isReply e) Just $ do
         thread <- m ^. #threads % at reid
         parentId <- thread ^. #parents % at (e ^. #eventId)
