3b01d43cfc15cc6c916fc4922f2614ffd5943e1c
Ralf Waldukat <ralf.waldukat@gmail.com>
2024-11-27 19:42:36 +0100

    feat(tabs): add responsive tabs with persistence, animation and custom styling

--- src/Tabs.hs
+++ src/Tabs.hs
@@ -1,22 +1,43 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module Tabs where
 
 import qualified Data.Text as T
 import Miso
+import qualified LocalStorage
 
-displayTabs :: (View action, View action) -> T.Text -> T.Text -> View action
-displayTabs (l, r) leftLabel rightLabel =
-  div_ [class_ "tabs"] $
-    [ div_
-        [class_ "left-tab"]
-        [ div_ [class_ "left-tab-header"] [text leftLabel],
-          l
-        ],
-      div_
-        [class_ "right-tab"]
-        [ div_ [class_ "right-tab-header"] [text rightLabel],
-          r
-        ]
+data TabConfig = TabConfig
+  { leftContent :: View action
+  , rightContent :: View action
+  , leftLabel :: T.Text
+  , rightLabel :: T.Text
+  , activeTab :: T.Text
+  , customStyle :: Maybe Attribute action
+  }
+
+data TabState = TabState
+  { currentTab :: T.Text
+  , isAnimating :: Bool
+  }
+
+displayTabs :: TabConfig -> View action
+displayTabs TabConfig{..} = 
+  div_ [class_ "tabs", customStyle `maybe` id] $ 
+    [ tabView "left-tab" leftLabel leftContent
+    , tabView "right-tab" rightLabel rightContent
     ]
+  where
+    tabView cls label content =
+      div_ [class_ cls, onClick (switchTab cls)]
+        [ div_ [class_ $ cls <> "-header"] [text label]
+        , div_ [class_ $ cls <> if isActive cls then " active" else ""] [content]
+        ]
 
+    isActive tab = activeTab == tab
+
+    switchTab :: T.Text -> action  
+    switchTab newTab = do
+      setState (\s -> s { currentTab = newTab, isAnimating = True })
+      LocalStorage.setItem "activeTab" newTab
+      setTimeout 300 $ setState (\s -> s { isAnimating = False })
