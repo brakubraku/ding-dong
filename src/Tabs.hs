e652e4b3a17b5dcb4a4185e6633c6caa4d7386d4
Ralf Waldukat <ralf.waldukat@gmail.com>
2024-11-27 19:52:49 +0100

    fixup

--- src/Tabs.hs
+++ src/Tabs.hs
@@ -1,19 +1,21 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module Tabs where
 
 import qualified Data.Text as T
 import Miso
 import qualified LocalStorage
+import Control.Monad.IO.Class (MonadIO)
 
 data TabConfig = TabConfig
-  { leftContent :: View action
-  , rightContent :: View action
+  { leftContent :: View Action
+  , rightContent :: View Action
   , leftLabel :: T.Text
   , rightLabel :: T.Text
   , activeTab :: T.Text
-  , customStyle :: Maybe Attribute action
+  , customStyle :: Maybe (Attribute Action)
   }
 
 data TabState = TabState
@@ -21,9 +23,15 @@ data TabState = TabState
   , isAnimating :: Bool
   }
 
-displayTabs :: TabConfig -> View action
+initialTabState :: TabState
+initialTabState = TabState
+  { currentTab = "left-tab"
+  , isAnimating = False
+  }
+
+displayTabs :: TabConfig -> View Action
 displayTabs TabConfig{..} = 
-  div_ [class_ "tabs", customStyle `maybe` id] $ 
+  div_ [class_ "tabs", maybe id id customStyle] $ 
     [ tabView "left-tab" leftLabel leftContent
     , tabView "right-tab" rightLabel rightContent
     ]
@@ -34,10 +42,12 @@ displayTabs TabConfig{..} =
         , div_ [class_ $ cls <> if isActive cls then " active" else ""] [content]
         ]
 
+    isActive :: T.Text -> Bool
     isActive tab = activeTab == tab
 
-    switchTab :: T.Text -> action  
+    switchTab :: MonadIO m => T.Text -> m ()
     switchTab newTab = do
       setState (\s -> s { currentTab = newTab, isAnimating = True })
       LocalStorage.setItem "activeTab" newTab
-      setTimeout 300 $ setState (\s -> s { isAnimating = False })
+      void $ setTimeout 300 $ setState (\s -> s { isAnimating = False })
+      where void = (>> return ())
