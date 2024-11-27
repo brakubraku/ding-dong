16f833b7e5cbd87ca186ccb35f70f11ec3b1de08
Ralf Waldukat <ralf.waldukat@gmail.com>
2024-11-27 19:36:31 +0100

    feat(wasi): refactor init into async function feat(wasi): improve error handling and logging feat(wasi): modernize ES imports

--- frontend/index.js
+++ frontend/index.js
@@ -1,22 +1,32 @@
-import { WASI, OpenFile, File, ConsoleStdout } from "./browser_wasi_shim/index.js";
-import ghc_wasm_jsffi from "./ghc_wasm_jsffi.js";
+const initWASI = async () => {
+  const { WASI, OpenFile, File, ConsoleStdout } = await import('./browser_wasi_shim/index.js');
+  const ghc_wasm_jsffi = await import('./ghc_wasm_jsffi.js');
 
-const args = [];
-const env = [];
-const fds = [
-  new OpenFile(new File([])), // stdin
-  ConsoleStdout.lineBuffered((msg) => console.log(`[WASI stdout] ${msg}`)),
-  ConsoleStdout.lineBuffered((msg) => console.warn(`[WASI stderr] ${msg}`)),
-];
-const options = { debug: true };
-const wasi = new WASI(args, env, fds, options);
+  const wasi = new WASI([], [], [
+    new OpenFile(new File([])),
+    ConsoleStdout.lineBuffered(msg => console.log(`[WASI] ${msg}`)),
+    ConsoleStdout.lineBuffered(msg => console.error(`[WASI] ${msg}`))
+  ], { debug: import.meta.env?.DEV });
 
-const instance_exports = {};
-const { instance } = await WebAssembly.instantiateStreaming(fetch("bin.wasm"), {
-  wasi_snapshot_preview1: wasi.wasiImport,
-  ghc_wasm_jsffi: ghc_wasm_jsffi(instance_exports),
-});
-Object.assign(instance_exports, instance.exports);
+  const exports = {};
+  try {
+    const { instance } = await WebAssembly.instantiateStreaming(
+      fetch('bin.wasm'),
+      {
+        wasi_snapshot_preview1: wasi.wasiImport,
+        ghc_wasm_jsffi: ghc_wasm_jsffi.default(exports)
+      }
+    );
 
-wasi.initialize(instance);
-await instance.exports.hs_start("something");
+    Object.assign(exports, instance.exports);
+    wasi.initialize(instance);
+    await instance.exports.hs_start();
+
+    return instance;
+  } catch (err) {
+    console.error('[WASI] Init failed:', err);
+    throw err;
+  }
+};
+
+initWASI().catch(console.error);
